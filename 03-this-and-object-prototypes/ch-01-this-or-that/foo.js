27d19a85f13b5f9f42041a344b2e2762a12690b1
John D. Aynedjian <johndavid5@yahoo.com>
2017-03-27 20:39:48 -0400

    Added foo.js..."proper" use of "this"...

---
+++ 03-this-and-object-prototypes/ch-01-this-or-that/foo.js
@@ -0,0 +1,25 @@
+/** Instead of avoiding "this", we embrace it... */
+function foo(num){
+
+	// keeps track of how many times "foo" 
+    // is called
+    // Nota Bene: "this" is actually "foo" now
+    // based on how "foo" is called (see below)
+	this.count++;
+
+	console.log( "foo(" , num , "), this.count =" , this.count ); 
+}
+
+foo.count = 0;
+
+var i;
+for( i = 0; i < 10; i++ ){
+	if( i > 5 ){
+		// Using 'call(...)', we ensure
+		// the "this" points at the function
+		// object ('foo') itself
+		foo.call( foo, i );
+	}
+}
+
+console.log("foo.count = ", foo.count );
